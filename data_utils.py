60b87e3856e3d31ba1a9b61150fa3ed11f412e7c
FrancisHu <87693204+Francis-Komizu@users.noreply.github.com>
2022-08-26 00:36:02 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -185,7 +185,7 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
 
         audiopaths_sid_text_new = []
         lengths = []
-        for audiopath, sid, text in self.audiopaths_sid_text:
+        for audiopath, sid, lang, text in self.audiopaths_sid_text:
             if self.min_text_len <= len(text) and len(text) <= self.max_text_len:
                 audiopaths_sid_text_new.append([audiopath, sid, text])
                 lengths.append(os.path.getsize(audiopath) // (2 * self.hop_length))
